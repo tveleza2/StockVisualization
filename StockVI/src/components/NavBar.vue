5657fbad966d693953855dbbb2438b5f160a3a9a
Tomas Velez Acosta <93617802+tveleza2@users.noreply.github.com>
2025-05-13 23:15:31 -0500

    Vue project created from scratch to configure tailwindcss

---
+++ StockVI/src/components/NavBar.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="flex justify-between items-center p-4 bg-[#C4E7D4]">
+      <img src="@/assets/logo.svg" class="w-1/20"></img>
+      <input v-model="search" placeholder="🔍 Search" class="border rounded px-2 py-1 w-1/3 text-center" />
+      <div class="text-sm text-gray-600">11:30 PT</div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue'
+  const search = ref('')
+  </script>
+  
--- StockVI/src/main.ts
+++ StockVI/src/main.ts
@@ -3,9 +3,16 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(PrimeVue,{
+    theme: {
+        preset: Aura
+    }
+})
 
 app.mount('#app')
