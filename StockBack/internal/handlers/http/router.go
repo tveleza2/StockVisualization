e8714f6bd3cc0c95d2907e251aa72d55ef1554ef
Tomas Velez Acosta <93617802+tveleza2@users.noreply.github.com>
2025-05-22 15:26:58 -0500

    Restructured database initialization and routing to make app.go clearer

--- StockBack/internal/handlers/http/router.go
+++ StockBack/internal/handlers/http/router.go
@@ -4,14 +4,23 @@ import (
 	"stock-app/internal/core/services"
 	"stock-app/internal/handlers"
 	"stock-app/internal/handlers/http/routes"
+	"stock-app/internal/repositories/db"
 
 	"github.com/gin-gonic/gin"
 )
 
-func NewRouter(actionService services.ActionService, ratingHistoricService services.RatingHistoricService) *gin.Engine {
+func NewTestRouter(actionService services.ActionService, ratingHistoricService services.RatingHistoricService) *gin.Engine {
 	router := gin.Default()
 	router.Use(handlers.CORSMiddleware())
 	routes.RegisterActionRoutes(router, actionService)
 	routes.RegisterRatingHistoricRoutes(router, ratingHistoricService)
+	return router
+}
+
+func NewRouter(schema *db.Schema) *gin.Engine {
+	router := gin.Default()
+	router.Use(handlers.CORSMiddleware())
+	routes.RegisterActionRoutes(router, schema.ActionService)
+	routes.RegisterRatingHistoricRoutes(router, schema.RHService)
 	return router
 }
